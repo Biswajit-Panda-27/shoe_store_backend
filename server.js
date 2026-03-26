5d4316404e8f57a96c4ae7b25a7f951ef5a6255c
Biswajit Panda <biswajit.panda@eatechpvtltd.com>
2026-03-27 02:41:47 +0530

    "/" added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = require('./app.js');
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
